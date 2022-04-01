35b995691b328658de0c26fb35801279e185e03a
Daniel Große <hallo@daniel-der-grosse.de>
2022-04-01 09:33:08 +0000

    feat: add boilerplate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import postcssjs from "postcss-js";
+import plugin from 'tailwindcss/plugin';
+
+
+const mainFunction = ({addBase, addComponents, addUtilities, config, postcss}) => {
+
+}
+
+export default plugin(mainFunction, {
+    
+})
